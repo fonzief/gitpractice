d70603ceb636dcc5abf822d24babf8f961709522
fonzief <nigerianprincess13@gmail.com>
2019-05-13 14:36:53 +0200

    it is done

---
+++ test.py
@@ -0,0 +1,18 @@
+
+list1 = ["f","o","n","z","i","e"]
+list2 = [1, 2, 3, 4, 5, 6]
+
+l = {key:value for key, value in zip(list1,list2)}
+print(l)
+
+d = {}
+for key, value in zip(list1 ,list2):
+    d[key] = value
+print(d)
+
+try:
+    print(x)
+except:
+    print("Syntaxerror:lid syntax")
+finally:
+    print("bye")
